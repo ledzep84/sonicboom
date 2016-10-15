5dfac0c76fb7ac3ca4cb6482159c21d7d8c7cae5
ledzep84 <ledzep84@users.noreply.github.com>
2016-10-15 12:37:14 +0800

    Create address-object-1_2.py

---
+++ address-object-1_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+file = open('ipadd.txt')
+#line = file.readlines()
+line = file.read().splitlines()
+
+i = 0
+
+while i < len(line):
+	print "address-object \"%s\"" % str(line[i])
+	print "host %s" % str(line[i])
+	print "zone WAN"
+	print "end"
+	i = i+1
+
+file.close()
